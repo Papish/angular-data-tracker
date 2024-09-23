c2d298234e8aed28b3f1e468bcb6e5ca491f9d73
Papish Limbu <lpapish97@gmail.com>
2024-09-23 22:53:42 +0545

    simple todo app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,57 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Todo } from './types';
+import { CommonModule } from '@angular/common';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, ReactiveFormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'angular-data-tracker';
+
+  newTodo = new FormControl('');
+
+  todos: Todo[] = [
+    {
+      id: 1,
+      isCompleted: false,
+      todo: 'Buy water',
+    },
+  ];
+
+  get completedTask() {
+    return this.todos.filter((todo) => todo.isCompleted).length;
+  }
+
+  get totalTask() {
+    return this.todos.length;
+  }
+
+  addTodo() {
+    this.todos.push({
+      id: Math.floor(Math.random() * 1000),
+      isCompleted: false,
+      todo: this.newTodo.value || '',
+    });
+    this.newTodo.setValue('');
+  }
+
+  updateTodoStatus(id: number) {
+    const index = this.todos.findIndex((todo) => todo.id === id);
+    if (index !== -1) {
+      this.todos[index].isCompleted = true;
+    }
+  }
+
+  deleteTodo(id: number) {
+    const index = this.todos.findIndex((todo) => todo.id === id);
+    if (index !== -1) {
+      this.todos.splice(index, 1);
+    }
+  }
 }
